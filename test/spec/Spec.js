505896820153e29ba37c7034ef174b82ca92c0f8
ajames72 <andrew.james@ajmebc.co.uk>
2015-09-26 08:07:37 +0100

    StartButton improvements
    Updating StartButton class to require less configuration detail

--- test/spec/Spec.js
+++ test/spec/Spec.js
@@ -23,5 +23,89 @@ define(['require', 'jQuery', 'PIXI', 'Wall', 'Brick', 'BrickTexture'], function 
 			expect(Wall.wallWidth()).toBe(100);
 		});
 	});
+	
+	describe("StartButton", function(){
+	
+		var StartButton = require('StartButton');
+		
+		describe("StartButton speed property", function(){
+			beforeEach(function(){
+
+			});
+			
+			afterEach(function(){
+			
+			});
+			
+			it("should test startbutton speed", function(){			
+				StartButton.initialize({
+					'speed': 4
+				});
+				expect(StartButton.speed).toBe(4);
+			});
+			
+			it("should test invalid startbutton speed values", function(){
+				StartButton.initialize({
+					'speed': -1
+				});
+				expect(StartButton.speed).toBe(1);
+			});
+			
+			it("should test invalid startbutton speed values", function(){
+				StartButton.initialize({
+					'speed': 'Not A Number'
+				});
+				expect(StartButton.speed).toBe(1);
+			});
+		});
+		
+		describe("setSpriteTextures array", function(){
+			beforeEach(function(){
+				StartButton.initialize({
+					'stage': null,
+					'spriteSheet': {
+						spriteSheetLoader : ["img/BrickHilight/brickhilight.json"],
+						spriteNameList : [
+							"brickHi_A1.png",
+							"brickHi_A2.png",
+							"brickHi_A3.png",
+							"brickHi_A4.png",
+							"brickHi_A5.png",
+							"brickHi_A6.png",
+							"brickHi_A7.png",
+							"brickHi_A8.png",
+						],
+					},
+					'positionX': 0,
+					'positionY': 0,
+					'setAnimationState': function(){
+
+					},
+					'speed': 4
+				});
+			});
+			
+			afterEach(function(){
+			
+			});
+			
+			it("should test startbutton speed", function(){			
+				expect(StartButton.speed).toBe(4);
+			});
+			
+			it("should test number of items in the spriteTextures array", function(){
+				var spriteTexturesArray = [];
+				var expectedSize = StartButton.spriteSheet.spriteNameList.length;
+				// spriteNameList * 2 - this includes the reverse fade,
+				expectedSize *= 2;
+				// * 4 - this is the speed of the fade
+				expectedSize *= StartButton.speed;
+				
+				spriteTexturesArray = StartButton.setSpriteTextures();
+				
+				expect(spriteTexturesArray.length).toBe(expectedSize);
+			});
+		});
+	});
 
 });
